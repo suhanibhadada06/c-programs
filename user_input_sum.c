da16770c6fd1975892e320f852870a839f39feca
suhanibhadada06 <suhanibhadada816@gmail.com>
2023-05-01 15:12:47 +0530

    file added

---
+++ user_input_sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+int num1,num2,result;
+printf("enter two integer: \n");
+scanf("%d" "%d",&num1,&num2);
+result=num1+num2;
+printf("the value of result is : %d",result);
+return 0;
+}
